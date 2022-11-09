65fcbdf7e1638e905c391dbbe6e3681bb3001043
lenmorld <lenmorld@gmail.com>
2022-11-08 19:58:32 -0500

    cleanup

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,7 +5,9 @@ echo "Hello world"
 echo "Image version: $1"
 export IMAGE_VERSION="$1"
 printenv
-envsubst < templates/template.yaml > kubernetes/deployment.yaml
+# apply env variables in template.yaml into deployment.yaml
+# using > will replace it every time, not append
+envsubst < templates/deployment.yaml > kubernetes/deployment.yaml
 echo "===== result of envsubst ===="
 cat kubernetes/deployment.yaml
 echo "===== end of envsubst"
